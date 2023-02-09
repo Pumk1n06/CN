53d7321255c1a062de1a8308ef10b4cf1d14c548
Poojith U Karadi <79163218+Pumk1n06@users.noreply.github.com>
2023-02-09 16:19:33 +0530

    Add files via upload

---
+++ DSender.java
@@ -0,0 +1,12 @@
+import java.net.*;  
+public class DSender{  
+  public static void main(String[] args) throws Exception {  
+    DatagramSocket ds = new DatagramSocket();  
+    String str = "Welcome java";  
+    InetAddress ip = InetAddress.getLocalHost(); 
+     
+    DatagramPacket dp = new DatagramPacket(str.getBytes(), str.length(), ip, 3000);  
+    ds.send(dp);  
+    ds.close();  
+  }  
+}  
